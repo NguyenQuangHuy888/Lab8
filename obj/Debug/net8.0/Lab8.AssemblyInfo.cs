7e3fa5b0ccb34478972cc522e522a76a7a77ed01
NguyenQuangHuy888 <huynqtd00446@fpt.edu.vn>
2025-02-28 09:53:07 +0700

    2 methods 6 tests

--- obj/Debug/net8.0/Lab8.AssemblyInfo.cs
+++ obj/Debug/net8.0/Lab8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71dd39d1383cfb8e4e76c3a912b651ec0cf47b26")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
